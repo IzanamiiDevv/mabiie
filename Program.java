7fe9b56e04598a926d7eecb28207084cb422af0a
izanami <120745634+IzanamiiDevv@users.noreply.github.com>
2024-11-14 15:42:34 +0000

    Gab

---
+++ Program.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hell");
+
+        int test = 20;
+
+        System.out.println(test);
+        
+    }
+}
